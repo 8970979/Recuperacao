154ebfbd7d741d2b58220b114f7c4dcb97f7ce83
8970979 <65878917+8970979@users.noreply.github.com>
2026-09-18 23:20:16 -0300

    Add files via upload

---
+++ maior_numero.php
@@ -0,0 +1,13 @@
+<?php
+
+$n1 = 10;
+$n2 = 18;
+$n3 = 4;
+
+ if ($n1 > $n2 && $n1 > $n3) {
+    echo "O numero maior é: $n1";
+}
+    elseif ($n2 > $n1 && $n2 > $n3) {
+    echo "O numero maior é: $n2";
+    }else
+    echo "O numero maior é: $n3";
